68c20ab8c7a84c54d3721608c5b3cb6eb0837282
Tyler Deem <tdeem6@gmail.com>
2015-09-25 16:40:15 -0700

    adds StudentsFactory.js and StudentsCtrl.js

---
+++ controllers/StudentsCtrl.js
@@ -0,0 +1,4 @@
+attendanceTaker.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
+});
---
+++ services/StudentsFactory.js
@@ -0,0 +1,40 @@
+attendanceTaker.factory('StudentsFactory', function StudentsFactory() {
+  var factory = {};
+  factory.students = [
+    {
+      name: "Peach",
+      present: false
+    },
+    {
+      name: "Zelda",
+      present: false
+    },
+    {
+      name: "JigglyPuff",
+      present: false
+    },
+    {
+      name: "Mario",
+      present: true
+    },
+    {
+      name: "Luigi",
+      present: false
+    },
+    {
+      name: "Toad",
+      present: false
+    },
+    {
+      name: "Bowser",
+      present: false
+    },
+    {
+      name: "Link",
+      present: false
+    },
+  ];
+
+
+  return factory
+});
